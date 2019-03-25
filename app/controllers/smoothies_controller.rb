180df486a40895dd48e24e39207d46efc49caf77
Megan Fago <meganlaynefago@gmail.com>
2019-03-25 18:59:42 -0400

    Added strong params and create

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -4,10 +4,26 @@ class SmoothiesController < ApplicationController
         @smoothies = Smoothie.all
         render json: @smoothies, status: :ok
     end
-    #tested above in postman
 
     def show 
         @smoothie = Smoothie.find(params[:id])
         render json: @smoothie, status: :ok
     end 
+    
+    def create 
+        @smoothie = Smoothie.new(smoothie_params)
+        if @smoothie
+            render json: @smoothie, status: :ok
+        else
+            render json: {errors: @smoothie.errors.full_messages},
+            status: :unprocessable_entity
+        end 
+    end 
+
+    private
+
+    def smoothie_params
+        params.permit(:name, :ingredients, :likes, :category, :created_by)
+    end 
+    
 end 
