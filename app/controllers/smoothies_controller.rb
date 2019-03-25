689e86ea63a921cfba55247491a95db1c2bb2d9d
Megan Fago <meganlaynefago@gmail.com>
2019-03-25 18:41:52 -0400

    2 Smoothie Seeds & 2 Smoothie Routes Made

--- app/controllers/smoothies_controller.rb
+++ app/controllers/smoothies_controller.rb
@@ -1,2 +1,13 @@
 class SmoothiesController < ApplicationController
-end
+
+    def index
+        @smoothies = Smoothie.all
+        render json: @smoothies, status: :ok
+    end
+    #tested above in postman
+
+    def show 
+        @smoothie = Smoothie.find(params[:id])
+        render json: @smoothie, status: :ok
+    end 
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Smoothie.create(name:"Kale and Ginger", ingredients: "Kale, Ginger, Spinach, Protein Powder, Almond Milk", likes: 5, category: "post workout", created_by: "Megan")
+Smoothie.create(name:"Strawberry and Peach", ingredients: "Strawberries, Peach, Chia Seeds, Protein Powder, Almond Milk", likes: 10, category: "preworkout", created_by: "Leigh")
